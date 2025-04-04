3d9c3cfcfc4fa660bfb2bc40cced967f436865b4
Claudio2000 <156216692+Claudio2000@users.noreply.github.com>
2025-04-04 11:10:02 +0100

    ok

--- lab1/predictor.py
+++ lab1/predictor.py
@@ -3,21 +3,21 @@ import pickle as p1
 import numpy as np
 import pandas as pd
 from sklearn import datasets, linear_model
+from sklearn.datasets import data
 from sklearn.metrics import mean_squared_error, r2_score
-data = pd.read_csv("./abaloneprevisao", sep=",") ## -- ou -- sep=",")
-data_x=input("introduza valores do wine\n")
-data=data_x.split(",")
+data_x=input("introduz valores do ablalone.data: \n")
+data=data_x.split(";")
 print(data)
 fmap_data = map(float, data)
 print(fmap_data)
-flist_data =np.array([list(fmap_data)])
+flist_data = list(fmap_data)
 print(flist_data)
-data1 = pd.read_csv('./abaloneprevisao', 'wb')
+data1 = pd.read_csv("abalone.data",   sep=";")
 data2=data1.iloc[:0,:11]
 data_preparation=pd.DataFrame([flist_data],columns=list(data2))
 out=data2
 for x in out:
     print(x,data_preparation[x].values)
-    loaded_model = p1.load(open('../abaloneprvisao', 'rb'))
-    y_pred=loaded_model.predict(data_preparation)
-    print("wine quality",int(y_pred))
+loaded_model = p1.load(open('abalone.data', 'rb'))
+y_pred=loaded_model.predict(data_preparation)
+print("abalone data ",int(y_pred))l
--- lab1/teste.py
+++ lab1/teste.py
@@ -5,12 +5,13 @@ import pandas as pd
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 data = pd.read_csv("./Dataset/abalone.data", sep=",") ## -- ou -- sep=",")
-evaluation_data=data[1001:]
-data_X=evaluation_data.iloc[:,1:8]
-data_Y=evaluation_data.iloc[:,8:9]
-print(type(evaluation_data))
-print(type(data_X))
-loaded_model = p1.load(open('./abaloneprevisao', 'rb'))
-print("Coefficients: \n", loaded_model.coef_)
-y_pred=loaded_model.predict(data_X)
-z_pred=y_pred-data_Y
+train_data=data[:1000]
+data_X=train_data.iloc[:,1:8]
+data_Y=train_data.iloc[:,8:9]
+#print(train_data.columns)
+print(data_X)
+print(data_Y)
+regr = linear_model.LinearRegression()
+preditor_linear_model=regr.fit(data_X, data_Y)
+preditor_Pickle = open('./abaloneprevisao', 'wb')
+print("Modelo criado")
